b3bc8ac6962b3120f006b474eb9dbf244ffbdd4b
Salma Khan <hussainsalma37@gmail.com>
2020-01-03 13:04:32 -0600

    adds form in home.html

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Widget(models.Model):
+    description = models.CharField(max_length=250)
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.description}'
